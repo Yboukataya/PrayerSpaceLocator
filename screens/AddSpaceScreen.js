ac103226ffe934a83ad3353b8847bb32c72dd09b
Mohamed Aly <mohamed.aly06@gmail.com>
2020-11-03 00:32:39 -0600

    Updates to export default function

--- screens/AddSpaceScreen.js
+++ screens/AddSpaceScreen.js
@@ -3,12 +3,10 @@ import { Text } from "react-native";
 
 import Screen from "../components/Screen";
 
-function AddSpaceScreen(props) {
+export default function AddSpaceScreen(props) {
   return (
     <Screen>
       <Text>Hello, world!</Text>
     </Screen>
   );
 }
-
-export default AddSpaceScreen;
