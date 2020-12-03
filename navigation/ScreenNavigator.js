063858af1f98ca211f02af32b518f48a53976da1
Mohamed Aly <mohamed.aly06@gmail.com>
2020-12-03 13:57:11 -0600

    Adds trivial link to navigator to test PennKey login

--- navigation/ScreenNavigator.js
+++ navigation/ScreenNavigator.js
@@ -5,11 +5,13 @@ import AddSpaceScreen from "../screens/AddSpaceScreen";
 import SpaceDetailScreen from "../screens/SpaceDetailScreen";
 import ListViewScreen from "../screens/ListViewScreen";
 import MapViewScreen from "../screens/MapViewScreen";
+import ToyPennKey from "../screens/ToyPennKey";
 
 const Stack = createStackNavigator();
 
 const ScreenNavigator = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Login" component={ToyPennKey} />
     <Stack.Screen name="Welcome" component={WelcomeScreen} />
     <Stack.Screen name="AddSpace" component={AddSpaceScreen} />
     <Stack.Screen name="SpaceDetail" component={SpaceDetailScreen} />
