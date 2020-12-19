fb234a55401c6d38e002a28c4f11c8c39f1abf21
Mohamed Aly <mohamed.aly06@gmail.com>
2020-12-19 10:25:30 -0600

    Adds renamed HomeNavigator file, forgot to add to last commit

---
+++ navigation/HomeNavigator.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import WelcomeScreen from "../screens/Welcome";
+import AddSpaceScreen from "../screens/AddSpaceScreen";
+import SpaceDetailScreen from "../screens/SpaceDetailScreen";
+import ListViewScreen from "../screens/ListViewScreen";
+import MapViewScreen from "../screens/MapViewScreen";
+import ToyPennKey from "../screens/ToyPennKey";
+
+const Stack = createStackNavigator();
+
+const HomeNavigator = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <Stack.Screen name="AddSpace" component={AddSpaceScreen} />
+    <Stack.Screen name="SpaceDetail" component={SpaceDetailScreen} />
+    <Stack.Screen name="ListView" component={ListViewScreen} />
+    <Stack.Screen name="MapView" component={MapViewScreen} />
+    <Stack.Screen name="Login" component={ToyPennKey} />
+  </Stack.Navigator>
+);
+
+export default HomeNavigator;
