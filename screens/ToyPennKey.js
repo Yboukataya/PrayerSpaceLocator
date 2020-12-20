130481a73f2531a170c0e4fd3b22593aba79408c
nabeelf <nfarooqui99@gmail.com>
2020-12-19 20:54:41 -0500

    hook issue

--- screens/ToyPennKey.js
+++ screens/ToyPennKey.js
@@ -5,13 +5,21 @@ import * as Google from "expo-google-app-auth";
 
 import AppText from "../components/AppText";
 import AppButton from "../components/AppButton";
+import getEnvVars from '../environment';
+const IOS_AUTH_ID = getEnvVars().ios_auth_key;
+//TODO: set android in environment.js
+//const ANDROID_AUTH_ID = getEnvVars().android_auth_key;
+let userName = "";
+let userEmail = "";
+let accessToken = "";
+
 
 async function signInWithGoogleAsync() {
   // setUserName, setUserEmail, setAccessToken
   try {
     const result = await Google.logInAsync({
-      androidClientId: "toBeImplemented",
-      iosClientId: "checkNabeelEmailForClientId",
+      androidClientId: "TODO",
+      iosClientId: IOS_AUTH_ID,
       scopes: ["profile", "email"],
     });
 
@@ -20,11 +28,19 @@ async function signInWithGoogleAsync() {
       console.log("name: " + result.user.name);
       console.log("fName: " + result.user.givenName);
       console.log("email: " + result.user.email + "\n");
-
+      if (!result.user.email.endsWith("upenn.edu")) {
+        console.log("error");
+        signOutWithGoogleAsync();
+        throw "Not a Penn email";
+      }
       // call the state hooks passed in the argument
-      let _ = setAccessToken(result.accessToken);
-      let _2 = setUserName(result.user.name);
-      let _3 = setUserEmail(result.user.email);
+      // let _ = setAccessToken(result.accessToken);
+      // let _2 = setUserName(result.user.name);
+      // let _3 = setUserEmail(result.user.email);
+
+      userName = result.user.name;
+      userEmail = result.user.email;
+      accessToken = result.user.accessToken;
 
       // make sure that the state hook
       console.log("OK");
@@ -43,20 +59,15 @@ async function signOutWithGoogleAsync() {
 
 function ToyPennKey(props) {
   // TODO: do we really need useState here? or can we just use a plain string?
-  const [userName, setUserName] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-  const [accessToken, setAccessToken] = useState("");
-
+  // const [userName, setUserName] = useState("");
+  // const [userEmail, setUserEmail] = useState("");
+  // const [accessToken, setAccessToken] = useState("");
   return (
     <View style={styles.container}>
       <AppText>Here's what you can do:</AppText>
       <AppButton
         title="login with Penn email"
-        onPress={signInWithGoogleAsync(
-          setUserName,
-          setUserEmail,
-          setAccessToken
-        )}
+        onPress={signInWithGoogleAsync}
       ></AppButton>
       <AppButton title="log out" onPress={signOutWithGoogleAsync}></AppButton>
       <AppText>Email: {userEmail}</AppText>
