33112ec60e16aa3d53caabc0d9510ef408c95af0
Mohamed Aly <mohamed.aly06@gmail.com>
2021-02-26 08:04:45 -0600

    Add config file with base URL

---
+++ config/backend-config.js
@@ -0,0 +1,7 @@
+// import axios from "axios";
+
+// export const backendApi = axios.create({
+//   baseUrl: "http://node3-env.eba-jm6mszh8.us-east-1.elasticbeanstalk.com/",
+// });
+
+export const baseUrl = "http://node3-env.eba-jm6mszh8.us-east-1.elasticbeanstalk.com/";
