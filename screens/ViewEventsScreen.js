0a7c71a93b35ad365636fe9531559cfc5efa53f4
Mohamed Aly <mohamed.aly06@gmail.com>
2021-03-01 12:31:13 -0600

    Switch double quotes to single

--- screens/ViewEventsScreen.js
+++ screens/ViewEventsScreen.js
@@ -1,66 +1,66 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Switch, TouchableOpacity, View } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Switch, TouchableOpacity, View } from 'react-native';
 
-import AppButton from "../components/AppButton";
-import AppEventList from "../components/AppEventList";
-import AppBuildingEvents from "../components/AppBuildingEvents";
-import AppTitle from "../components/AppTitle.js";
-import AppText from "../components/AppText";
-import Screen from "../components/Screen";
-import "localstorage-polyfill";
-import axios from "axios";
-import { baseUrl } from "../config/backend-config.js";
+import AppButton from '../components/AppButton';
+import AppEventList from '../components/AppEventList';
+import AppBuildingEvents from '../components/AppBuildingEvents';
+import AppTitle from '../components/AppTitle.js';
+import AppText from '../components/AppText';
+import Screen from '../components/Screen';
+import 'localstorage-polyfill';
+import axios from 'axios';
+import { baseUrl } from '../config/backend-config.js';
 
-import AppEventListing from "../components/AppEventListing";
+import AppEventListing from '../components/AppEventListing';
 
 let eventsN = [
   {
     id: 1,
-    building: "VanPelt Library",
-    space: "DUNGEON",
-    eventName: "Maghrib at VP",
+    building: 'VanPelt Library',
+    space: 'DUNGEON',
+    eventName: 'Maghrib at VP',
     date: new Date(2021, 2, 15, 19, 30, 0, 0),
   },
   {
     Eventid: 2,
-    Building: "Houston",
-    Space: "SPARC",
-    eventName: "ZUHR with bros only",
+    Building: 'Houston',
+    Space: 'SPARC',
+    eventName: 'ZUHR with bros only',
     date: new Date(2021, 2, 15, 13, 30, 0, 0),
   },
   {
     Eventid: 3,
-    Building: "Houston",
-    Space: "SPARC",
-    eventName: "ladies only",
+    Building: 'Houston',
+    Space: 'SPARC',
+    eventName: 'ladies only',
     date: new Date(2021, 2, 15, 13, 35, 0, 0),
   },
   {
     Eventid: 4,
-    Building: "Engineering",
-    Space: "electrical switch room",
-    Name: "sorry, engineering only",
+    Building: 'Engineering',
+    Space: 'electrical switch room',
+    Name: 'sorry, engineering only',
     Date: new Date(2021, 2, 14, 13, 40, 0, 0),
   },
   {
     Eventid: 4,
-    Building: "VanPelt Library",
-    Space: "electrical switch room",
-    Name: "sorry, engineering only",
+    Building: 'VanPelt Library',
+    Space: 'electrical switch room',
+    Name: 'sorry, engineering only',
     Date: new Date(2021, 2, 14, 13, 40, 0, 0),
   },
   {
     Eventid: 4,
-    Building: "Engineering",
-    Space: "electrical switch room",
-    Name: "sorry, engineering only",
+    Building: 'Engineering',
+    Space: 'electrical switch room',
+    Name: 'sorry, engineering only',
     Date: new Date(2021, 2, 14, 13, 40, 0, 0),
   },
   {
     Eventid: 4,
-    Building: "Engineering",
-    Space: "electrical switch room",
-    Name: "sorry, engineering only",
+    Building: 'Engineering',
+    Space: 'electrical switch room',
+    Name: 'sorry, engineering only',
     Date: new Date(2021, 2, 14, 13, 40, 0, 0),
   },
 ];
@@ -78,7 +78,7 @@ function ViewEventsScreen({ navigation, route }) {
   };
 
   useEffect(() => {
-    fetch(baseUrl + "events")
+    fetch(baseUrl + 'events')
       .then((response) => response.json())
       .then((json) => {
         setEvents(json.data);
@@ -87,7 +87,7 @@ function ViewEventsScreen({ navigation, route }) {
   }, []);
 
   useEffect(() => {
-    fetch(baseUrl + "buildings")
+    fetch(baseUrl + 'buildings')
       .then((response) => response.json())
       .then((json) => {
         setEvents(json.data);
@@ -97,26 +97,18 @@ function ViewEventsScreen({ navigation, route }) {
 
   // TODO: where do we go after adding a new event?
 
-  console.log("events: ", myEventsState[0]);
+  console.log('events: ', myEventsState[0]);
   return (
     <Screen style={{ flex: 1, padding: 20 }}>
       <View style={styles.viewSelect}>
         <TouchableOpacity onPress={() => setViewByBuilding(!viewByBuilding)}>
-          <AppText
-            customStyle={
-              viewByBuilding ? styles.subTitleSelected : styles.subTitle
-            }
-          >
+          <AppText customStyle={viewByBuilding ? styles.subTitleSelected : styles.subTitle}>
             Events By Building
           </AppText>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => setViewByBuilding(!viewByBuilding)}>
-          <AppText
-            customStyle={
-              viewByBuilding ? styles.subTitle : styles.subTitleSelected
-            }
-          >
+          <AppText customStyle={viewByBuilding ? styles.subTitle : styles.subTitleSelected}>
             All Events Today
           </AppText>
         </TouchableOpacity>
@@ -124,7 +116,7 @@ function ViewEventsScreen({ navigation, route }) {
 
       <View style={styles.headingContainer}>
         <AppText customStyle={styles.title}>
-          {viewByBuilding ? "Buildings" : "Today's Events"}
+          {viewByBuilding ? 'Buildings' : "Today's Events"}
         </AppText>
       </View>
 
@@ -135,9 +127,7 @@ function ViewEventsScreen({ navigation, route }) {
         ) : (
           <>
             <View style={styles.switchContainer}>
-              <AppText customStyle={{ paddingRight: 10 }}>
-                View My Events Only
-              </AppText>
+              <AppText customStyle={{ paddingRight: 10 }}>View My Events Only</AppText>
               <Switch onValueChange={toggleSwitch} value={viewMyEventsOnly} />
             </View>
             <View style={styles.eventListContainer}>
@@ -159,47 +149,47 @@ function ViewEventsScreen({ navigation, route }) {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
+    alignItems: 'center',
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   headingContainer: {
-    alignItems: "center",
+    alignItems: 'center',
     // borderColor: "blue",
     // borderWidth: 2,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   eventListContainer: {
     flex: 1,
-    width: "90%",
+    width: '90%',
     // borderColor: "blue",
     // borderWidth: 2,
   },
   subTitle: {
-    color: "blue",
+    color: 'blue',
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   subTitleSelected: {
-    fontStyle: "italic",
+    fontStyle: 'italic',
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   switchContainer: {
     // flex: 1,
-    alignItems: "center",
-    flexDirection: "row",
+    alignItems: 'center',
+    flexDirection: 'row',
     // justifyContent: "flex-end",
     paddingTop: 10,
   },
   title: {
     fontSize: 30,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   viewSelect: {
     // flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     paddingBottom: 10,
   },
 });
