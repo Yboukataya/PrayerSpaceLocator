c641f3e154a9577a398cc44d2b3da7749661e1db
Mohamed Aly <mohamed.aly06@gmail.com>
2021-03-16 09:04:55 -0500

    Remove comment

--- screens/EventDetailScreen.js
+++ screens/EventDetailScreen.js
@@ -10,8 +10,6 @@ import AppText from '../components/AppText';
 import Screen from '../components/Screen';
 
 export default function EventDetailScreen({ route }) {
-  // console.log('CHECK EVENT OBJECT: \n', route.params.event);
-
   const navigation = useNavigation();
 
   return (
